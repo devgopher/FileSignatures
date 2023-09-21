e8f9655321b5dcb4ee8526359d0c8b51554ed5e7
stone1985 <igevdo@mail.ru>
2023-09-21 18:24:07 +0300

    - mp3 signatures

--- src/FileSignatures/Formats/Mpeg3Id3v2.cs
+++ src/FileSignatures/Formats/Mpeg3Id3v2.cs
@@ -3,8 +3,8 @@
     /// <summary>
     /// Specifies the format of a MPEG-1 Audio Layer 3 (MP3) audio file
     /// </summary>
-    public class Mpeg3 : FileFormat
+    public class Mpeg3Id3v2 : FileFormat
     {
-        public Mpeg3() : base(new byte[] { 0x49, 0x44, 0x33 }, "audio/mpeg", "mp3") { }
+        public Mpeg3Id3v2() : base(new byte[] { 0x49, 0x44, 0x33 }, "audio/mpeg", "mp3") { }
     }
 }
---
+++ src/FileSignatures/Formats/Mpeg3NoId3_4.cs
@@ -0,0 +1,9 @@
+﻿namespace FileSignatures.Formats;
+
+/// <summary>
+/// Mp3 with no Id4
+/// </summary>
+public class Mpeg3NoId3_4 : FileFormat
+{
+    public Mpeg3NoId3_4() : base(new byte[] { 0xFF, 0xE3 }, "audio/mpeg", "mp3") { }
+}
