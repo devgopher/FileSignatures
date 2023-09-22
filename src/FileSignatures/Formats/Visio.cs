cda382034c6dc4df63acc237b16c9eaff59041f0
devgopher <igevdo@mail.ru>
2023-09-22 15:12:51 +0300

    - cleaning

--- src/FileSignatures/Formats/Visio.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FileSignatures.Formats
-{
-    public class Visio : OfficeOpenXml
-    {
-        public Visio() : base("visio/document.xml", "application/vnd.visio", "vsdx")
-        {
-        }
-    }
-}
