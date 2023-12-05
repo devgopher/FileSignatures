2bb1beebd8578b70b96d28b91ae54b4c310d9d74
Igor Evdokimov <igor.evdokimov@external.tele2.ru>
2023-12-05 11:27:33 +0300

    - resharper

--- Formats/3GPP.cs
+++ Formats/3GPP.cs
@@ -1,7 +1,7 @@
 namespace FileSignatures.Formats
 {
     /// <summary>
-    /// Specifies the format of a 3rd Generation Partnership Project 3GPP multimedia files (3GG, 3GP, 3G2)
+    ///     Specifies the format of a 3rd Generation Partnership Project 3GPP multimedia files (3GG, 3GP, 3G2)
     /// </summary>
     public class ThreeGPP : Isobmff
     {
